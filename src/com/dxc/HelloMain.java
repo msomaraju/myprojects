2fabffaf5ba82296e38b61edb67988ad0ae978be
somarajum <somaraju.java6@gmail.com>
2017-09-19 10:08:09 +0530

    project commited.

---
+++ src/com/dxc/HelloMain.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello hi how are you");
+
+	}
+
+}
+
